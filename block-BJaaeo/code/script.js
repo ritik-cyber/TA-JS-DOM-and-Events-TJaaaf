ffdfc8fe72f6a5052c17687d2c98059924e1710e
ritik-cyber <ritikdhingra03@gmail.com>
2021-07-06 15:58:58 +0530

    done

--- block-BJaaeo/code/script.js
+++ block-BJaaeo/code/script.js
@@ -1,1 +1,23 @@
-function createTodoList(data = []) {}
+let display = document.querySelector(".display");
+let allBtn = document.querySelectorAll("button");
+
+let intialValue = 0;
+display.innerText = intialValue;
+
+function handleBtnClick(event) {
+  if (event.target.classList.contains("equal")) {
+    display.innerText = eval(display.innerText);
+    return;
+  }
+  if (event.target.classList.contains("clear")) {
+    display.innerText = intialValue;
+    return;
+  }
+  if (display.innerText == intialValue) {
+    display.innerText = event.target.innerText;
+  } else {
+    display.innerText += event.target.innerText;
+  }
+}
+
+allBtn.forEach((btn) => btn.addEventListener("click", handleBtnClick));
